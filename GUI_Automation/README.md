5ffb099a2a690756cb4b329c543b84f35ccf64d0
JiaYu07 <a0965178617@gmail.com>
2019-12-29 16:57:49 +0800

    gui-automation

---
+++ GUI_Automation/README.md
@@ -0,0 +1,10 @@
+How to create the script automatic start?
+1. open your gnome-session-properties
+
+![](search.png)
+2. command 
+
+![](new.png)
+```
+python3  {your path}/main.py
+```
--- GUI_Automation/main.py
+++ GUI_Automation/main.py
@@ -3,7 +3,7 @@ import tkinter as tk
 import webbrowser
 
 def open_github():
-    webbrowser.open()
+    webbrowser.open(url="https://github.com/")
 
 
 def open_vscode():
@@ -11,12 +11,20 @@ def open_vscode():
 
 
 def open_playlist():
-    path=/home/crystal/桌面/google_code_in/GUI_Automation/music_list/play_list.m3u
-     os.system("xdg-open",path) 
+    path= "/home/crystal/桌面/google_code_in/GUI_Automation/music_list.m3u8"
+    os.system("xdg-open "+ path) 
      
 
 def main():
+    gui= tk.Tk()
+    gui.title("Python Automation App")
+    gui.geometry("480x360")
+    gui.configure(background="white")
+    toGitHub_btn = tk.Button(gui, text='Open GitHub',activebackground="grey",command=open_github).pack()
+    toVscode_btn = tk.Button(gui, text='Open Vscode',activebackground="grey",command=open_vscode).pack()
+    playMusic_btn = tk.Button(gui, text='Play Music List',activebackground="grey",command=open_playlist).pack()
 
+    gui.mainloop()
 
 
 if __name__=="__main__":

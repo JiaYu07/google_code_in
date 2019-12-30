ffda2c3879b367b8ccbdde23178f4a4f461f1f44
JiaYu07 <a0965178617@gmail.com>
2019-12-31 00:17:30 +0800

    python resize

--- image_resize/main.py
+++ image_resize/main.py
@@ -2,51 +2,48 @@ import glob
 import os
 import re
 from PIL import Image
-#input_dir=input()
-#file_list = os.listdir('/'+input_dir)
-infile=""
-file=''
-im=''
+
 def main():
-    global infile, file, im
     files = []
     for ext in ('/*.jpeg', '/*.png', '/*.jpg'):
-        files.extend(glob.glob(input_dir+ext))
-
-    #print(files)
+        files.extend(glob.glob('/home/crystal/**/'+input_dir+ext,recursive=True))        
+    
     for infile in files:
         file, ext = os.path.splitext(infile)
         im = Image.open(infile)
+        form=(im.format).lower()
+        filename = infile.split("/")
+        fname=filename[-1].split('.')[0]
+       #print(fname)
+        x_s=0
+        y_s=0
         if ext == u'.png' or u'.jpeg' or u'.jpg':
-            (x,y)=im.size
-            #print(x,y)
-            y_s=300
-            x_s=x/(y_s/300)                
-            if  (x_s>400):
-                x_s=400            
-            im= im.resize((x_s,y_s),Image.ANTIALIAS)
-            saving()            
+            (x,y)=im.size            
+           #print(x,y)
+            if (x<400 and y<400) :
+                x_s=x
+                y_s=y
+            elif y>x or y==x:  
+                ratio=float(x)/y
+                y_s=400
+                x_s=int(y_s*ratio)                
+            elif x< y:
+                ratio=float(y)/x
+                x_s=400
+                y_s=int(x_s*ratio)  
 
             physical_size=64*1024
-            size_tmp=im.getsize()
-            q=100
-            while size_tmp > physical_size and q>0 :
-                out = im.resize(im.size, Image.ANTIALIAS) 
-                saving(q)
-                size_tmp=im.getsize()
-                print(size_tmp)
-                q-=5               
+            size_tmp=os.path.getsize(infile)
+            q=95
 
+            while size_tmp > physical_size and q>5 :                
+                size_tmp=os.path.getsize(infile)
+                im.save(fname,form,quality=q)
+                #print(size_tmp)
+                q-=3  
 
-def saving(q=100):
-    global infile, file, im
-
-    if re.match(".png$",infile):
-        im.save(file+".png" , "png",quality=q)
-    if re.match(".jpg$",infile):
-        im.save(file+".jpg" , "jpg",quality=q)
-    if re.match(".jpeg$",infile):
-        im.save(file+".jpeg" , "jpeg",quality=q)
+            im= im.resize((x_s,y_s),Image.ANTIALIAS)      
+            im.save(infile,quality=q)    
 
 if __name__ == '__main__':
      input_dir=input()

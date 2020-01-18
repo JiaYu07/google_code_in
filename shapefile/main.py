d01050f9fda69fc1b31230e77ca6ac48727f87d4
JiaYu07 <a0965178617@gmail.com>
2020-01-18 21:22:57 +0800

    update

--- shapefile/main.py
+++ shapefile/main.py
@@ -1,67 +1,16 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
-import os
-#% plt inline
-#os.getcwd()
-#os.path.join(os.getcwd,"")
-#get=open(r'taipei/roads.shp')
-#桌面/google_code_in/shapefile/taipei/roads.shp
-fp="/home/crystal/桌面/google_code_in/shapefile/"
-taipei_roads = gpd.read_file(fp+"taipei/shape/roads.shp")
-taipei_natural = gpd.read_file(fp+"taipei/shape/natural.shp")
-taipei_buildings= gpd.read_file(fp+"taipei/shape/buildings.shp")
-#print(gdf.head())
-seoul_roads=gpd.read_file(fp+"seoul/shape/roads.shp")
-seoul_natural=gpd.read_file(fp+"seoul/shape/natural.shp")
-seoul_buildings=gpd.read_file(fp+"seoul/shape/buildings.shp")
 
-kao_roads=gpd.read_file(fp+"kaoshuing/shape/roads.shp")
-kao_natural=gpd.read_file(fp+"kaoshuing/shape/natural.shp")
-kao_buildings=gpd.read_file(fp+"kaoshuing/shape/buildings.shp")
+basic="/home/crystal/桌面/google_code_in/shapefile/"
+city=["taipei","seoul","kaoshuing","tokyo","beijing"]
+element=["buildings","landuse","natural","places","points","railways","roads","waterways"]
 
-tokyo_roads=gpd.read_file(fp+"tokyo/shape/roads.shp")
-tokyo_natural=gpd.read_file(fp+"tokyo/shape/natural.shp")
-tokyo_buildings=gpd.read_file(fp+"tokyo/shape/buildings.shp")
-
-beijing_roads=gpd.read_file(fp+"beijing/shape/roads.shp")
-beijing_natural=gpd.read_file(fp+"beijing/shape/natural.shp")
-beijing_buildings=gpd.read_file(fp+"beijing/shape/buildings.shp")
-
-fig, axes1 = plt.subplots(figsize=(150,150))
-taipei_roads.plot(ax=axes1, color='blue')
-taipei_natural.plot(ax=axes1, color='green')
-taipei_buildings.plot(ax=axes1, color='red')
-axes1.set_title("taipei")
-plt.show()
-
-fig, axes2 = plt.subplots(figsize=(150,150))
-seoul_roads.plot(ax=axes2, color='blue')
-seoul_natural.plot(ax=axes2, color='g')
-seoul_buildings.plot(ax=axes2, color='red')
-axes2.set_title("seoul")
-plt.show()
-
-fig, axes3 = plt.subplots(figsize=(150,150))
-kao_roads.plot(ax=axes3, color='blue')
-kao_natural.plot(ax=axes3, color='g')
-kao_buildings.plot(ax=axes3, color='red')
-axes3.set_title("kao")
-plt.show()
-
-fig, axes4 = plt.subplots(figsize=(150,150))
-tokyo_roads.plot(ax=axes4, color='blue')
-tokyo_natural.plot(ax=axes4, color='green')
-tokyo_buildings.plot(ax=axes4, color='red')
-axes4.set_title("tokyo")
-plt.show()
-
-fig, axes5 = plt.subplots(figsize=(150,150))
-beijing_roads.plot(ax=axes5, color='blue')
-beijing_natural.plot(ax=axes5, color='g')
-beijing_buildings.plot(ax=axes5, color='red')
-axes5.set_title("beijing")
-plt.show()
-
-
-#gdf.plot()
-#plt.show()
+for i in city:
+    fig, axes= plt.subplots(figsize=(150,150))
+    axes.set_title(i)
+    tmp=""
+    for j in element:
+        tmp=basic+i+"/shape/"+j+".shp"
+        msg=gpd.read_file(tmp)   
+        msg.plot(ax=axes,cmap="gist_earth")
+    plt.show()

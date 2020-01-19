7e694f35986a9e67cc3f3c02e93eaec88f338fd2
JiaYu07 <a0965178617@gmail.com>
2020-01-19 12:17:19 +0800

    update

--- shapefile/main.py
+++ shapefile/main.py
@@ -1,16 +1,22 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
+import os
+import pathlib
 
-basic="/home/crystal/桌面/google_code_in/shapefile/"
+basic= os.getcwd()
 city=["taipei","seoul","kaoshuing","tokyo","beijing"]
 element=["buildings","landuse","natural","places","points","railways","roads","waterways"]
 
-for i in city:
-    fig, axes= plt.subplots(figsize=(150,150))
-    axes.set_title(i)
-    tmp=""
-    for j in element:
-        tmp=basic+i+"/shape/"+j+".shp"
-        msg=gpd.read_file(tmp)   
-        msg.plot(ax=axes,cmap="gist_earth")
-    plt.show()
+def main():
+    for i in city:
+        fig, axes= plt.subplots(figsize=(150,150))
+        axes.set_title(i)
+        tmp=""
+        for j in element:
+            tmp=basic+"/cities/"+i+"/shape/"+j+".shp"
+            msg=gpd.read_file(tmp)   
+            msg.plot(ax=axes,cmap="gist_earth")
+        plt.show()
+        
+if __name__ == "__main__":
+    main()
